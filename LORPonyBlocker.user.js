7104a04e39d7e5d35a375b91ee89eb21f9b88c10
oneliner-lor <oiefgyoq@mail.ru>
2013-09-25 22:36:50 +0400

    kote ftw
    Ponies replaced with cats from upyachka.ru.

--- LORPonyBlocker.user.js
+++ LORPonyBlocker.user.js
@@ -17,101 +17,21 @@
 (function($){
 
  var ponyfaglist=[
- 'a1batross',
- 'aidan',
- 'Akamanah',
- 'amomymous',
- 'AnimusPEXUS',
- 'Anonymous',
- 'Antimatter',
- 'AptGet',
- 'atrus',
- 'avertex',
- 'Bagrov',
- 'bsdfun',
- 'Chaser_Andrey',
- 'Cooler',
- 'com',
- 'Copycat',
- 'cryptohedge',
- 'Dark_SavanT',
- 'Darth_Revan',
- 'dearboy',
- 'Deneb',
- 'derlafff',
- 'devpony',
- 'Dispetcher14',
- 'DoctorSinus',
- 'drBatty',
- 'druganddrop-2',
- 'ekzotech',
- 'emulek',
- 'essir',
- 'evilmanul',
- 'Extraterrestrial',
- 'Falcon-peregrinus',
- 'Flyn',
- 'fornlr',
- 'HiddenComplexity',
- 'hizel',
- 'Hoodoo',
- 'IceAlchemist',
- 'Igorrr',
- 'Ingvarr',
- 'IPR',
- 'ishido',
- 'ishitori',
- 'kasthack',
- 'kiverattes',
- 'ktk',
- 'ktulhu666',
- 'Landgraf',
- 'Legioner',
- 'Le_Raux',
- 'Lonli-Lockley',
- 'Lorchanin',
- 'melkor217',
- 'mephistopheles',
- 'mopsene',
- 'morse',
- 'NetForHack',
- 'Newlifer',
- 'nihil',
- 'Old_Hamster',
- 'OldWiseCat',
- 'olibjerd',
- 'PaxthonFettel',
- 'pevzi',
- 'procoder99',
- 'proud_anon',
- 'quowah',
- 'reliktt',
- 'rikardoac',
- 'Romaboy',
- 'rtvd',
- 'StalinEXE',
- 'thelonelyisland',
- 'Thero',
- 'uin',
- 'vazgen05',
- 'veneperkele',
- 'voronin',
- 'x0r',
- 'Yaourt',
- 'yaspol'
  ];
- 
+ var koteStopList=[0,6,14,26,27,28,29,31,32,51,53,57,87,92,93,101,144];
+ var noKoteHere=154;
+
  function readSetting(name) {
   return localStorage.getItem(name) == 'true';
  }
- 
+
  function toggleSetting(name) {
   var result = readSetting(name);
   result = !result;
   localStorage.setItem(name, result ? 'true' : 'false');
   return result;
  }
- 
+
  function displaySetting(name, defaultChecked) {
   var result = readSetting(name);
   result = (result && !defaultChecked) || (!result && defaultChecked);
@@ -119,56 +39,33 @@
  }
 
  var settingsTab={
-  'fill_ponies': 'Заменять аватарки не-понифагов поняшами',
+  'fill_ponies': 'Заменять аватарки не-понифагов котами',
   'hide_ponies': 'Скрывать аватарки понифагов'
  };
- 
- var poniAvs=[
-  [
-   'applejack',
-   80
-  ],
-  [
-   'fluttershy',
-   72
-  ],
-  [
-   'pinkie_pie',
-   127
-  ],
-  [
-   'rainbow_dash',
-   176
-  ],
-  [
-   'rarity',
-   117
-  ],
-  [
-   'twilight_sparkle',
-   117
-  ]
- ];
- 
- function pad3(number) {
-  var s='';
-  s+=number/100|0;
-  s+=(number/10)%10|0;
-  s+=number%10;
-  return s;
- }
- 
- function randPony(){
-  var name=poniAvs[(Math.random()*6)|0];
-  var up=name[0].indexOf('_'),s_name;
-  if (up>-1){
-   s_name=name[0].substring(0,up)+name[0].substring(up+1);
-  } else {
-   s_name=name[0];
+
+ String.prototype.hashCode = function(){
+     var hash = 0, i, char;
+     if (this.length == 0) return hash;
+     for (i = 0, l = this.length; i < l; i++) {
+         char  = this.charCodeAt(i);
+         hash  = ((hash<<5)-hash)+char;
+         hash |= 0; // Convert to 32bit integer
+     }
+     return hash;
+ };
+
+ function getKoteUrl(nick){
+  var n = Math.abs(nick.hashCode()) % noKoteHere;
+  while (koteStopList.indexOf(n) != -1) {
+    n = (n+1) % noKoteHere;
   }
-  return 'http://www.bronyland.com/goodies/avatars/'+s_name+'/100/'+name[0]+'_avatar_'+pad3((Math.random()*name[1])|0+1)+'.png';
+
+  var s=''+n;
+  if (n<10) s='0'+s;
+  var name='http://www.upyachka.ru/img/kot/'+s+'.gif';
+  return name;
  }
- 
+
  if (/settings/.test(window.location.href)){
   var baseHr=(($('#profileForm hr:eq(0)').parent()).parent())
   baseHr.after(
@@ -196,9 +93,9 @@
   }
  };
 
- var ponycache=localStorage.getItem('ponycache');
- ponycache=ponycache?JSON.parse(ponycache):{};
-  
+ var kotecache=localStorage.getItem('kotecache');
+ kotecache=kotecache?JSON.parse(kotecache):{};
+
  var checkNick=function(){
   var nick=$(this).text();
   for (var i=0;i<ponyfaglist.length;i++){
@@ -210,21 +107,21 @@
    }
   }
   if (readSetting('fill_ponies')){
-   for (pony in ponycache){
+   for (pony in kotecache){
     if (pony==nick) {
-     var av=ponycache[pony];
+     var av=kotecache[pony];
      break;
     }
    }
    if (av==undefined){
-    var av=randPony();
-    ponycache[nick]=av;
+    var av=getKoteUrl(nick);
+    kotecache[nick]=av;
    }
    (($(this).closest('.msg-container')).find('.userpic img')).attr('src',av).width(100).height(100);
   }
  };
  $('article.msg a[itemprop=\'creator\']').each(checkNick);
- localStorage.setItem('ponycache',JSON.stringify(ponycache));
+ localStorage.setItem('kotecache',JSON.stringify(kotecache));
 })(jQuery);
 
 
